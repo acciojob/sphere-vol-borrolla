d8ffd6f32ae44720614e48b35b01385defbf03be
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-04-01 16:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 function volume_sphere() {
     //Write your code here
+	 event.preventDefault();
+	
 	// Retrieve the radius value entered by the user
-    let radiusInput = document.getElementById("radius").value;
-
+    const radiusInput = document.getElementById("radius").value;
 	// Convert the input to a number and validate it
-    let radius = parseFloat(radiusInput);
+    const radius = parseFloat(radiusInput);
     if (isNaN(radius) || radius < 0) {
         // Display an error if the input is invalid
         document.getElementById("volume").innerText = "NAN";
         return;
     }
-
-	console.log("radius: ", radius);
 	
 	///calculate volume of sphere
-	let volume = (4 / 3) * Math.PI * Math.POW(radius, 3);
+	const volume = (4 / 3) * Math.PI * Math.POW(radius, 3);
     // Round the volume to four decimal places
-    volume = volume.toFixed(4);
-
-	console.log("volume: ", volume);
+   const  volume = volume.toFixed(4);
 
 	document.getElementById("volume").value = volume;
+	
 } 
 
+
 window.onload = function() {
-    document.getElementById('MyForm').onsubmit = function(e) {
-        e.preventDefault();
-        volume_sphere();
-    }
+    document.getElementById('MyForm').onsubmit = volume_sphere;
 }
+
 	
 	
